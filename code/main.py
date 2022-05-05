e45b6668096135f0009b988c6df8e19c0fa52ec8
finnschule <finn.schule28@gmail.com>
2022-05-05 15:43:37 -0400

    new stuff

--- code/main.py
+++ code/main.py
@@ -43,10 +43,26 @@ networks_monthly = mismatch_estimation(dfLabor_market_monthly,param_networks,gue
 networks_monthly.mHP(10,'networks_monthly',600)
 networks_monthly.sector_level('networks_monthly',600)
 
+##### Decomposition and Robustness ##### 
 # Constant λα, different e across industries
 param_cλα = {'A':A,'φ':φ,'λ':np.ones_like(λ),'α':np.ones_like(α),'θ':θ,'η':η,'mfunc':m_cd,'mufunc':mu_cd,'Lfunc':Lstar,'objective':ustar_objective}
 constantλα_monthly = mismatch_estimation(dfLabor_market_monthly,param_cλα,guessrange=0.01,ntrue=2,tol=1e-8)
 constantλα_monthly.mHP(10,'constantλα_monthly',600)
+
+# Constant λ
+param_cλ = {'A':A,'φ':φ,'λ':np.ones_like(λ),'α':α,'θ':θ,'η':η,'mfunc':m_cd,'mufunc':mu_cd,'Lfunc':Lstar,'objective':ustar_objective}
+constantλ_monthly = mismatch_estimation(dfLabor_market_monthly,param_cλ,guessrange=0.01,ntrue=2,tol=1e-8)
+constantλ_monthly.mHP(10,'constantλ_monthly',600)
+
+#100*λ
+param_c100λ = {'A':A,'φ':φ,'λ':100*λ,'α':α,'θ':θ,'η':η,'mfunc':m_cd,'mufunc':mu_cd,'Lfunc':Lstar,'objective':ustar_objective}
+constant100λ_monthly = mismatch_estimation(dfLabor_market_monthly,param_c100λ,guessrange=0.01,ntrue=2,tol=1e-8)
+constant100λ_monthly.mHP(10,'constant100λ_monthly',600)
+
+# Constant α
+param_cα = {'A':A,'φ':φ,'λ':λ,'α':np.ones_like(α),'θ':θ,'η':η,'mfunc':m_cd,'mufunc':mu_cd,'Lfunc':Lstar,'objective':ustar_objective}
+constantα_monthly = mismatch_estimation(dfLabor_market_monthly,param_cα,guessrange=0.01,ntrue=2,tol=1e-8)
+constantα_monthly.mHP(10,'constantα_monthly',600)
 
 # Constant e, different λα across industries
 param_ce = {'A':A,'φ':φ,'λ':λ,'α':α,'θ':θ,'η':η,'mfunc':m_cd,'mufunc':mu_cd,'Lfunc':Lstar,'objective':ustar_objective}
@@ -55,7 +71,12 @@ dfLabor_market_monthly_constant_e.e = dfLabor_market_monthly.e.mean()
 constante_monthly = mismatch_estimation(dfLabor_market_monthly_constant_e,param_cλα,guessrange=0.01,ntrue=2,tol=1e-8)
 constante_monthly.mHP(10,'constante_monthly',600)
 
-# Figures 
+# Constant α = 1, constant e
+param_αeq1 = {'A':A,'φ':φ,'λ':λ,'α':np.ones_like(α),'θ':θ,'η':η,'mfunc':m_cd,'mufunc':mu_cd,'Lfunc': Lones,'objective':ustar_objective}
+αeq1_monthly = mismatch_estimation(dfLabor_market_monthly_constant_e,param_αeq1,guessrange=0.01,ntrue=2,tol=1e-8)
+αeq1_monthly.mHP(10,'constantα_monthly',600)
+
+##### Figures ##### 
 fig_aggregate_mismatch, ax = plt.subplots(1,1,dpi=600)
 plt.rcParams['font.size'] = '8'
 ax.plot(sahin_monthly.output.index,sahin_monthly.output.mismatch_trend,'-k',label='Horizontal Economy')
@@ -79,9 +100,11 @@ ax2.legend()
 plt.savefig('code/output/mismatch_index_comparison_2ax.png')
 
 fig_network_mismatch_decomp, ax = plt.subplots(1,1,dpi=600)
-plt.rcParams['font.size'] = '8'
+plt.rcParams['font.size'] = '6'
 ax.plot(networks_monthly.output.index,networks_monthly.output.mismatch_trend,'-k',label='Network Economy')
 ax.plot(constantλα_monthly.output.index,constantλα_monthly.output.mismatch_trend,'--r',label='Constant λα Across Sectors')
+ax.plot(constantλ_monthly.output.index,constantλ_monthly.output.mismatch_trend,'--g',label='Constant λ Across Sectors')
+ax.plot(constantα_monthly.output.index,constantα_monthly.output.mismatch_trend,'--y',label='Constant α Across Sectors')
 ax.plot(constante_monthly.output.index,constante_monthly.output.mismatch_trend,'--b',label='Constant Existing Employment Across Sectors')
 ax.set_xlabel('Date')
 ax.set_ylabel('Mismatch Index')
@@ -97,5 +120,23 @@ ax.set_ylabel('Mismatch Index')
 ax.legend()
 plt.savefig('code/output/mismatch_index_comparison_constante.png')
 
+fig_aggregate_mismatch, ax = plt.subplots(1,1,dpi=600)
+plt.rcParams['font.size'] = '8'
+ax.plot(networks_monthly.output.index,networks_monthly.output.mismatch_trend,'-k',label='Network Economy')
+ax.plot(constant100λ_monthly.output.index,constant100λ_monthly.output.mismatch_trend,'--r',label='Network Economy with 100λ')
+ax.set_xlabel('Date')
+ax.set_ylabel('Mismatch Index')
+ax.legend()
+plt.savefig('code/output/mismatch_index_comparison_100lambda.png')
+
+
+fig_aggregate_mismatch_αeq1, ax = plt.subplots(1,1,dpi=600)
+plt.rcParams['font.size'] = '8'
+ax.plot(sahin_monthly.output.index,sahin_monthly.output.mismatch_trend,'-k',label='Horizontal Economy')
+ax.plot(αeq1_monthly.output.index,αeq1_monthly.output.mismatch_trend,'--r',label='Network Economy with α = 1')
+ax.set_xlabel('Date')
+ax.set_ylabel('Mismatch Index')
+ax.legend()
+plt.savefig('code/output/mismatch_index_comparison_alpha_eq1.png')
 
 print('done')
