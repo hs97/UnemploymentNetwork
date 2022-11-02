33cb8446e905ce0ebd45d41e0291d39f08239641
finnschule <finn.schule28@gmail.com>
2022-11-02 00:33:38 -0400

    new code to match new equations

---
+++ new_code/functions/cobb_douglas_network.py
@@ -0,0 +1,56 @@
+import numpy as np
+from functions.helpers_price_quantities import pricing, wages, sectoral_output, aggregate_real_output
+from functions.helpers_labor_market import theta, elasticity_labor_demand
+
+class cobb_douglas_network:
+    def __init__(self, Omega, elasticity_Dc, elasticity_fN, elasticity_Qtheta, tau, curlyF, elasticity_wtheta, elasticity_wA):
+        #Initializes production network. All variables assumed to be at initial equilibrium values.
+        self.Omega = Omega # matrix of production elasticities to intermediate goods for network at initial equilibrium
+        self.elasticity_Dc = elasticity_Dc # elasticity final goods production to intermediate goods at initial equilibrium
+        self.elasticity_fN = elasticity_fN # elasticity of production to labor input at intitial equilibrium
+        self.elasticity_Qtheta = elasticity_Qtheta # elasticity of vacacy filling rate to tightness at initial equilibrium
+        self.tau = tau # recruiter producer ratio at initial equilibrium
+        self.curlyF = curlyF # recruiter producer ratio at initial equilibrium
+        self.elasticity_wtheta = elasticity_wtheta # elasticity of wages to tightness at initial equilibrium
+        self.elasticity_wA= elasticity_wA# elasticity of wages to productivity at initial equilibrium
+
+        # Following variables are pinned down by the restrictions implied by Cobb-Douglas technology
+        self.elasticity_lambdatheta = np.zeros_like(self.elasticity_wtheta) #elasticity of Domar weights to tightness
+        self.elasticity_lambdaA = np.zeros_like(self.elasticity_wA) #elasticity of Domar weights to technology
+        self.elasticity_elasticityfNtheta = np.zeros_like(self.elasticity_wtheta) # elasticity of elasticity of production to labor input to tightness
+        self.elasticity_elasticityfNA= np.zeros_like(self.elasticity_wA) # elasticity of elasticity of production to labor input to productivity
+   
+        self.dloglambda = np.zeros_like(elasticity_Dc) # Domar weights are fixed
+        self.dlogelasticity_fN = np.zeros_like(elasticity_Dc) # Sector level production elasticities are fixed
+        self.dlogelasticity_Dc = np.zeros_like(elasticity_Dc) # Final production elasticities are fixed
+
+        # Leontief inverse
+        self.Psi = np.linalg.inv(self.Omega)
+
+    def shocks(self,dlogA,dlogH,H,L,U):
+        # Calculates response of prices, quantities, labor market to shocks to productivity (dlogA) or the size of the labor force (dlogH)
+        self.dlogA = dlogA
+        self.dlogH = dlogH
+        self.H = H
+        self.L = L
+        self.U = U
+        
+        self.elasticity_Ldtheta = elasticity_labor_demand(self.elasticity_fN, self.Psi, self.elasticity_wtheta, self.elasticity_lambdatheta,self.elasticity_elasticityfNtheta) 
+        self.elasticity_LdA = elasticity_labor_demand(self.elasticity_fN, self.Psi, self.elasticity_wA, self.elasticity_lambdaA,self.elasticity_elasticityfNA) 
+        
+        # Tightness and prices
+        self.dlogtheta = theta(self.dlogA, self.dlogH, self.curlyF, self.elasticity_Ldtheta, self.elasticity_LdA)
+        self.dlogw = wages(self.dlogtheta,self.dlogA,self.elasticity_wtheta, self.elasticity_wA)
+        self.dlogp = pricing(self.dlogw, self.dlogtheta, self.dlogA, self.elasticity_fN, self.elasticity_Qtheta, self.tau, self.Psi)
+        
+        # Output
+        self.dlogy = sectoral_output(self.dlogp,self.dloglambda,self.dlogelasticity_fN,self.elasticity_fN,self.Psi)
+        self.dlogY = aggregate_real_output(self.dlogp,self.dlogelasticity_fN,self.dlogelasticity_Dc,self.elasticity_Dc,self.elasticity_fN,self.Psi)
+
+        # Unemployment
+        self.dlogL = np.diag(self.curlyF) @ self.dlogtheta
+        self.dlogU = np.diag(np.power(U,-1)) @ (np.diag(H) @ self.dlogH - np.diag(L) @ self.dlogL)
+
+    def wage_elasticities(self,elasticity_wtheta, elasticity_wA):
+        # Allows quick changes to wage elasticities
+        self.elasticity_wtheta, self.elasticity_wA = elasticity_wtheta, elasticity_wA
---
+++ new_code/functions/helpers_labor_market.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+##### Tightness #####
+def theta(dlogA, dlogH, curlyF, elasticity_Ldtheta, elasticity_LdA):
+    theta_coeff = np.linalg.inv(np.diag(curlyF)-elasticity_Ldtheta)
+    dlogtheta = theta_coeff @ elasticity_LdA @ dlogA - theta_coeff @ dlogH
+    return dlogtheta
+
+##### elasticity of labor demand wrt theta ##### 
+def elasticity_labor_demand(elasticity_fN, Psi, elasticity_wtheta, elasticity_lambdatheta, elasticity_elasticityfNtheta):
+    I = np.eye(Psi.shape[0])
+    elasticity_fN_coeff = np.linalg.inv(I-np.diag(elasticity_fN) @ Psi) @ np.diag(elasticity_fN) 
+    elasticity_Ldtheta = elasticity_lambdatheta + (I + elasticity_fN_coeff) @ elasticity_elasticityfNtheta - elasticity_wtheta
+    return elasticity_Ldtheta
+
+##### Generating other necessary labor market inputs in case of Cobb-Douglas matching #####
+# will need different functions here for different functional forma assumptions about matching
+
+def gen_curlyF_CD(theta,eta,phi,s):
+    return s/(s + phi*np.power(theta,1-eta)) * (1-eta)
+
+def gen_elasticity_Qtheta_CD(theta, eta):
+    return -eta * np.ones_like(theta) 
+
+def gen_tau_CD(theta,eta,phi,s,r):
+    return r * s / (phi * np.power(theta,-eta) - r * s)
